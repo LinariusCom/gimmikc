9cf630dbe535877d2726d0fa321845f306062708
LinariusCom <66804235+LinariusCom@users.noreply.github.com>
2021-05-21 16:32:46 -0700

    Add files via upload

---
+++ BTS fan.js
@@ -0,0 +1,15 @@
+<html> 
+    <head>
+   <title>I like k-pop; BTS specifically.
+   </title>
+   </head>
+Here is a list of some songs by them i like in a top 5 thingy.
+<li>1. Butter
+    2. Life goes on
+    3. Boy with Luv
+    4. Serendipity
+    5. Filter</li>
+<p></p>Some of theese songs have been in my head for <b>WEEKS</b>. <ul><b>since i first listened to them. Ive been having borderline spotify loops and i honestly think i need rehab 
+from this bands music.</b></ul>
+<Img.src>https://tenor.com/view/bts-vbts-taehyung-chocchipkookies-tae-gif-12772692</Img.src>
+</html>
