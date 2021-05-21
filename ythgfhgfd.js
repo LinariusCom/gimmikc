8f1ceacab7a4b7957154805f762520cef1e26e62
LinariusCom <66804235+LinariusCom@users.noreply.github.com>
2021-05-21 16:07:40 -0700

    Add files via upload

---
+++ ythgfhgfd.js
@@ -0,0 +1,5 @@
+<body>
+<p>I am basil; making this line somehow because i just started learning javascript today.</p>
+<p>Im typing this shIt out because <b>I HAVE TO FOR FUCKING PRACTICE BECAUSE I WANNA GET TO KNOW CODING MOREJDKFLFJD.</b> </p>
+<p>Excpect some retarded shit to come out in the future alr.</p>
+<p>from and to you, Basil.</p></body>
